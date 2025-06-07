974027f243745799660b13a2182404f957fb1af6
Rahadian Nur Irfan Saepullah <rahadian.nur@pactindo.com>
2025-06-07 11:21:42 +0700

    SETUP RUNNING SPRING-BOOT IN JAVA VERSION 17

---
+++ src/main/java/com/ngulik/springboot/NgulikSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ngulik.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgulikSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgulikSpringbootApplication.class, args);
+	}
+
+}
